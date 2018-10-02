d7aa885593b0f1aeea792fa9265609fea13aa4ae
Daniel B <busisd@carleton.edu>
2018-10-01 23:26:37 -0500

    Finished api-test.py

--- using-apis/api-test.py
+++ using-apis/api-test.py
@@ -17,16 +17,25 @@ def main():
     data_from_server = urllib.request.urlopen(base_url).read()
     string_from_server = data_from_server.decode('utf-8')
     deaths_data_list = json.loads(string_from_server)
-    print(deaths_data_list)
+    if len(sys.argv) == 1:
+        print(deaths_data_list)
+    else:
+        death_stat = deaths_data_list[0]
+        print("State: " + death_stat['state'])
+        print("Year: " + death_stat['year'])
+        print("Cause of Death: " + death_stat['cause_name'])
+        print("Deaths: " + death_stat['deaths'])
+        print("Age-adjusted Death Rate: " + death_stat['aadr'])
     
-def _print_argv_error():    raise Exception("Incorrect command line arguments!")
+def _print_argv_error():
+    raise Exception("Incorrect command line arguments!")
 
 if __name__ == '__main__':
     print("This program outputs stroke deaths in Minnesota, optionally for a given year.")
     print("Correct usage: api-test.py optional: [year]")
     print("Optional arguments:")
     print("[year]: year the deaths occured in. Must be between 1999 and 2016.")
-
+    print()
     if (len(sys.argv) != 1 and len(sys.argv) != 2):
         _print_argv_error()
     if len(sys.argv) == 2 and (int(sys.argv[1]) < 1999 or int(sys.argv[1]) > 2016):
