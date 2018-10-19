983df31869dda2277db48ec2ac01f39fdde29445
Daniel B <busisd@carleton.edu>
2018-10-19 12:56:48 -0500

    me_data_converter.py now works in line with our revised table structure

--- webapp/sql_data_test/me_data_converter.py
+++ webapp/sql_data_test/me_data_converter.py
@@ -4,14 +4,19 @@ import re
 import csv
 import os
 
+categories_list = [
+	"GDP per capita, PPP (current international $)",
+	"GDP per capita (current US$)",
+	"GDP, PPP (constant 2011 international $)",
+	"GDP (current US$)"
+]
+
 def load_from_data_folder(folder):
 	file_list = os.listdir(folder)
-
-	gdp_usd_data = []
-	gdp_ppp_data = []
-	gdp_usd_capita_data = []
-	gdp_ppp_capita_data = []
+	list_of_all_countries = []
+	
 	for file_name in file_list:
+		cur_country_list = []
 		csv_file = open(folder+'/'+file_name)
 		reader = csv.reader(csv_file)
 		
@@ -24,47 +29,39 @@ def load_from_data_folder(folder):
 				year_chart = row
 				continue
 				
+			if len(cur_country_list) == 0:
+				cur_country_list.append(row[0])
+				
 			cur_data_category = row[2]
-			if cur_data_category == "GDP per capita, PPP (current international $)":
-				for i in range(4, len(row)-1):
-					country_name = row[0]
-					year = int(year_chart[i])
-					gdp_ppp_capita_value = row[i]
-					gdp_ppp_capita_data.append([country_name, year, gdp_ppp_capita_value])
-			elif cur_data_category == "GDP per capita (current US$)":
-				for i in range(4, len(row)-1):
-					country_name = row[0]
-					year = int(year_chart[i])
-					gdp_usd_capita_value = row[i]
-					gdp_usd_capita_data.append([country_name, year, gdp_usd_capita_value])
-			elif cur_data_category == "GDP, PPP (constant 2011 international $)":
-				for i in range(4, len(row)-1):
-					country_name = row[0]
-					year = int(year_chart[i])
-					gdp_ppp_value = row[i]
-					gdp_ppp_data.append([country_name, year, gdp_ppp_value])
-			elif cur_data_category == "GDP (current US$)":
-				for i in range(4, len(row)-1):
-					country_name = row[0]
-					year = int(year_chart[i])
-					gdp_usd_value = row[i]
-					gdp_usd_data.append([country_name, year, gdp_usd_value])
-	
-	return (gdp_ppp_capita_data, gdp_ppp_data, gdp_usd_capita_data, gdp_usd_data)
+			if cur_data_category in categories_list:
+				cur_category_list = []
+				cur_category_list.append(row[2])
+				cur_category_list.extend(row[4:])
+				cur_country_list.append(cur_category_list)
+						
+		list_of_all_countries.append(cur_country_list)
+	return (list_of_all_countries)
 
-def save_table(cur_data, file_name):
-	output_file = open(file_name, 'w')
-	writer = csv.writer(output_file)
-	for entry in cur_data:
-		writer.writerow(entry)
-	output_file.close()
+
+def save_table (list_of_all_countries):
+	for  list in list_of_all_countries:
+		filename = list_of_all_countries[0]+".csv"
+		
+		output_file = open(filename, 'w')
+		writer = csv.writer(output_file)
+		for entry in cur_data[1:]:
+			writer.writerow(entry)
+		output_file.close()
 
 if __name__ == '__main__':
-	gdp_ppp_capita_data, gdp_ppp_data, gdp_usd_capita_data, gdp_usd_data = load_from_data_folder('source_data')
-	save_table(gdp_usd_data, 'gdp_usd_data.csv')
-	save_table(gdp_usd_capita_data, 'gdp_usd_capita_data.csv')
-	save_table(gdp_ppp_data, 'gdp_ppp_data.csv')
-	save_table(gdp_ppp_capita_data, 'gdp_ppp_capita_data.csv')
+	all_countries_list = load_from_data_folder('source_data')
+	#save_table(gdp_usd_data, 'gdp_usd_data.csv')
+	#save_table(gdp_usd_capita_data, 'gdp_usd_capita_data.csv')
+	#save_table(gdp_ppp_data, 'gdp_ppp_data.csv')
+	#save_table(gdp_ppp_capita_data, 'gdp_ppp_capita_data.csv')
+	
+	#[["Bahrain", rest of list ] ["Arab World", rest of list] [] [] []]
+	print(all_countries_list[0]	)
 	
 #Note: To put tables into SQL, use:
 #\copy data_gdp_usd from 'gdp_usd_data.csv' DELIMITER ',' CSV NULL as ''
