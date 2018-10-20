9ebaee5ccaad0d86b92d494405a5ae24b3cabc08
Daniel B <busisd@carleton.edu>
2018-10-19 21:55:46 -0500

    Fixed me_data_converter.py

--- webapp/sql_data_test/me_data_converter.py
+++ webapp/sql_data_test/me_data_converter.py
@@ -14,39 +14,39 @@ categories_list = [
 	"GDP per capita (current US$)",
 	"GDP, PPP (constant 2011 international $)",
 	"GDP (current US$)",
-    "Central government debt, total (% of GDP)",
-    "Consumer price index (2010 = 100)",
-    "Unemployment, total (% of total labor force) (national estimate)",
-    "Military expenditure (% of GDP)",
-    "Literacy rate, adult total (% of people ages 15 and above)",
-    "Literacy rate, adult male (% of males ages 15 and above)",
-    "Literacy rate, adult female (% of females ages 15 and above)",
-    "Literacy rate, youth male (% of males ages 15-24)",
-    "Literacy rate, youth (ages 15-24), gender parity index (GPI)",
-    "Literacy rate, youth female (% of females ages 15-24)",
-    "Mortality rate, adult, male (per 1,000 male adults)",
-    "Mortality rate, adult, female (per 1,000 female adults)",
-    "Mortality rate, infant (per 1,000 live births)",
-    "School enrollment, secondary, male (% gross)",
-    "School enrollment, secondary, female (% gross)",
-    "School enrollment, secondary (% gross)",
-    "Educational attainment, at least completed short-cycle tertiary, population 25+, total (%) (cumulative)",
-    "Educational attainment, at least completed short-cycle tertiary, population 25+, male (%) (cumulative)",
-    "Educational attainment, at least completed short-cycle tertiary, population 25+, female (%) (cumulative)",
-    "Educational attainment, at least Master's or equivalent, population 25+, total (%) (cumulative)",
-    "Educational attainment, at least Master's or equivalent, population 25+, male (%) (cumulative)",
-    "Educational attainment, at least Master's or equivalent, population 25+, female (%) (cumulative)",
-    "Educational attainment, Doctoral or equivalent, population 25+, total (%) (cumulative)",
-    "Educational attainment, Doctoral or equivalent, population 25+, male (%) (cumulative)",
-    "Educational attainment, Doctoral or equivalent, population 25+, female (%) (cumulative)",
-    "Educational attainment, at least Bachelor's or equivalent, population 25+, total (%) (cumulative)",
-    "Educational attainment, at least Bachelor's or equivalent, population 25+, male (%) (cumulative)",
-    "Educational attainment, at least Bachelor's or equivalent, population 25+, female (%) (cumulative)",
-    "Population, male (% of total)",
-    "Population, male",
-    "Population, female (% of total)",
-    "Population, female",
-    "Population, total",
+	"Central government debt, total (% of GDP)",
+	"Consumer price index (2010 = 100)",
+	"Unemployment, total (% of total labor force) (national estimate)",
+	"Military expenditure (% of GDP)",
+	"Literacy rate, adult total (% of people ages 15 and above)",
+	"Literacy rate, adult male (% of males ages 15 and above)",
+	"Literacy rate, adult female (% of females ages 15 and above)",
+	"Literacy rate, youth male (% of males ages 15-24)",
+	"Literacy rate, youth (ages 15-24), gender parity index (GPI)",
+	"Literacy rate, youth female (% of females ages 15-24)",
+	"Mortality rate, adult, male (per 1,000 male adults)",
+	"Mortality rate, adult, female (per 1,000 female adults)",
+	"Mortality rate, infant (per 1,000 live births)",
+	"School enrollment, secondary, male (% gross)",
+	"School enrollment, secondary, female (% gross)",
+	"School enrollment, secondary (% gross)",
+	"Educational attainment, at least completed short-cycle tertiary, population 25+, total (%) (cumulative)",
+	"Educational attainment, at least completed short-cycle tertiary, population 25+, male (%) (cumulative)",
+	"Educational attainment, at least completed short-cycle tertiary, population 25+, female (%) (cumulative)",
+	"Educational attainment, at least Master's or equivalent, population 25+, total (%) (cumulative)",
+	"Educational attainment, at least Master's or equivalent, population 25+, male (%) (cumulative)",
+	"Educational attainment, at least Master's or equivalent, population 25+, female (%) (cumulative)",
+	"Educational attainment, Doctoral or equivalent, population 25+, total (%) (cumulative)",
+	"Educational attainment, Doctoral or equivalent, population 25+, male (%) (cumulative)",
+	"Educational attainment, Doctoral or equivalent, population 25+, female (%) (cumulative)",
+	"Educational attainment, at least Bachelor's or equivalent, population 25+, total (%) (cumulative)",
+	"Educational attainment, at least Bachelor's or equivalent, population 25+, male (%) (cumulative)",
+	"Educational attainment, at least Bachelor's or equivalent, population 25+, female (%) (cumulative)",
+	"Population, male (% of total)",
+	"Population, male",
+	"Population, female (% of total)",
+	"Population, female",
+	"Population, total",
 
 ]
 
@@ -55,12 +55,12 @@ def load_from_data_folder(folder):
 	list_of_all_countries = []
 	
 	for file_name in file_list:
-        
+		
 		csv_file = open(folder+'/'+file_name)
-        reader = csv.reader(csv_file)
-        currnet_country_data = []
-        list_of_years = []
-        
+		reader = csv.reader(csv_file)
+		cur_country_data = []
+		list_of_years = []
+		
 		for row in reader:
 			if len(row) < 4:
 				continue
@@ -69,25 +69,25 @@ def load_from_data_folder(folder):
 				list_of_years = row
 				continue
 				
-			if len(currnet_country_data) == 0:
-				currnet_country_data.append(row[0])
+			if len(cur_country_data) == 0:
+				cur_country_data.append(row[0])
 				
 			current_data_category = row[2]
 			if current_data_category in categories_list:
 				current_category_list = []
 				current_category_list.append(row[2])
 				current_category_list.extend(row[4:])
-				current_country_list.append(current_category_list)
+				cur_country_data.append(current_category_list)
 						
-		list_of_all_countries.append(current_country_data)
+		list_of_all_countries.append(cur_country_data)
 	return (list_of_all_countries)
 
 
 def save_tables(list_of_all_countries):
 	
-    for  list in list_of_all_countries:
+	for  list in list_of_all_countries:
 		
-        filename = list[0]+"_data.csv"		
+		filename = "data_output/"+list[0]+"_data.csv"		
 		output_file = open(filename, 'w')
 		writer = csv.writer(output_file)
 		for entry in list[1:]:
