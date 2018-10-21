7e63925bd3a20158672752acfeebd840b1144b5d
Daniel B <busisd@carleton.edu>
2018-10-20 23:15:54 -0500

    api might work with some get arguments now, needs testing

--- webapp/sql_data_test/api.py
+++ webapp/sql_data_test/api.py
@@ -16,24 +16,41 @@ def hello():
 	return 'No data requested.'
 
 @app.route('/country/data/')
-def data_for_one_country(country_name):
+def data_for_one_country():
 	data_list = []
-	column_string = make_column_string(start_year, end_year)
-	select_string = "SELECT "+column_string+" FROM "+country_name+"_data"
+	
+	start_year = flask.request.args.get('start_year')
+	end_year = flask.request.args.get('end_year')
+
+	list_of_years_requested = make_years_list(start_year, end_year)
+	column_string = make_column_string(list_of_years_requested)
+	select_string = "SELECT "+column_string+" FROM annual_data"
 	
 	stat_id = flask.request.args.getlist('stat_id')
-	if len(stat_id) > 0:
-		select_string += " WHERE stat_id = "+str(stat_id[0])
-		for stat in stat_id[1:]:
-			select_string += " OR stat_id = "+str(stat)
+	country_id = flask.request.args.getlist('country_id')
+	stat_name = flask.request.args.getlist('stat_name')
+	country_name = flask.request.args.getlist('country_name')
 	
-	else:
-		#This line works:
-		#SELECT year_1960 FROM algeria_data,stat_ids WHERE stat_ids.stat_name LIKE 'GDP%' AND algeria_data.stat_id=stat_ids.stat_id;
-		stat_name = flask.request.args.get('stat_name')
-		if stat_name is not None:
-			select_string += " WHERE stat_id = "+str(stat_id[0])
-		
+	optional_args = False
+	if len(stat_id) > 0
+			or len(country_id) > 0
+			or len(stat_name) > 0
+			or len(country_name) > 0:
+		select_string += " WHERE"
+		optional_args = True
+
+	for stat in stat_id:
+		select_string += " stat_id = "+str(stat)+" OR"
+	for stat in country_id:
+		select_string += " country_id = "+str(stat)+" OR"
+	
+	#This line works:
+	#SELECT year_1960 FROM algeria_data,stat_ids WHERE stat_ids.stat_name LIKE 'GDP%' AND algeria_data.stat_id=stat_ids.stat_id;
+	
+	#Trims off trailing "OR"
+	if optional_args:
+		select_string = select_string[:-3]
+	
 	try:
 		cursor.execute(select_string)
 	except Exception as e:
@@ -45,14 +62,23 @@ def data_for_one_country(country_name):
 	
 	return json.dumps(data_list)
 
-def make_column_string(start_year, end_year):
+def make_years_list(start_year, end_year):
+	if start_year is None:
+		start_year = 1960
+	if end_year is None:
+		end_year = 2017
+	year_list = []
+	for i in range(start_year, end_year+1):
+		year_list.append("year_"+str(i))
+	return year_list
+
+def make_column_string(year_list):
 	column_string = "country_id,stat_id"
-	for i in range(start_year, end_year):
-		column_string+=",year_"+str(i)
+	for i in year_list:
+		column_string+=","+i
 	return column_string
 	
-
-    @app.route('/country/')
+@app.route('/country/')
 def return_all_countries_and_ids():
     pass
     
