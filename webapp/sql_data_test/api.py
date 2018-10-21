0358e461560dc190b90ba890654e87379ee6461d
Daniel B <busisd@carleton.edu>
2018-10-21 01:33:28 -0500

    Api works even better now

--- webapp/sql_data_test/api.py
+++ webapp/sql_data_test/api.py
@@ -15,6 +15,8 @@ app = flask.Flask(__name__)
 def hello():
 	return 'No data requested.'
 
+	
+#You can't use start_year/end_year and year arguments
 @app.route('/country/data/')
 def data_for_one_country():
 	data_list = []
@@ -31,42 +33,66 @@ def data_for_one_country():
 	stat_name = flask.request.args.getlist('stat_name')
 	country_name = flask.request.args.getlist('country_name')
 	
+	if len(stat_name) > 0:
+		select_string += ",stats"
+	if len(country_name) > 0:
+		select_string += ",countries"		
+	
 	optional_args = False
-	if len(stat_id) > 0
+	if (len(stat_id) > 0
 			or len(country_id) > 0
 			or len(stat_name) > 0
-			or len(country_name) > 0:
+			or len(country_name) > 0):
 		select_string += " WHERE"
-		optional_args = True
+		optional_args = True		
 
-	for stat in stat_id:
-		select_string += " stat_id = "+str(stat)+" OR"
-	for stat in country_id:
-		select_string += " country_id = "+str(stat)+" OR"
+	for id in stat_id:
+		select_string += " stat_id = "+id+" AND"
+	for id in country_id:
+		select_string += " country_id = "+id+" AND"
+	for name in stat_name:
+		select_string += " LOWER(stats.name) LIKE LOWER('%"+name+"%') AND"
+		select_string += " annual_data.stat_id=stats.id AND"
+	for name in country_name:
+		select_string += " LOWER(countries.name) LIKE LOWER('%"+name+"%') AND"
+		select_string += " annual_data.country_id=countries.id AND"
 	
-	#This line works:
-	#SELECT year_1960 FROM algeria_data,stat_ids WHERE stat_ids.stat_name LIKE 'GDP%' AND algeria_data.stat_id=stat_ids.stat_id;
+	#These lines works:
+	#SELECT year_1960 FROM algeria_data,stat_ids WHERE stats.name LIKE 'GDP%' AND annual_data.stat_id=stats.id;
+	#SELECT country_id,stat_id,year_1960 FROM annual_data,stats where stats.id=annual_data.stat_id AND stats.name LIKE 'Population, male%';
 	
-	#Trims off trailing "OR"
+	#Trims off trailing " AND"
 	if optional_args:
-		select_string = select_string[:-3]
+		select_string = select_string[:-4]
 	
 	try:
+		print(select_string)
 		cursor.execute(select_string)
 	except Exception as e:
 		print(e)
 		exit()
 	
 	for row in cursor:
-		data_list.append(row)
+		cur_row_dict = {}
+		cur_row_dict['country_id'] = row[0]
+		cur_row_dict['stat_id'] = row[1]
+		for i in range(len(list_of_years_requested)):
+			cur_row_dict[list_of_years_requested[i]] = row[i+2]
+		data_list.append(cur_row_dict)
 	
 	return json.dumps(data_list)
-
+	
+#Must return a SORTED list of years!
 def make_years_list(start_year, end_year):
 	if start_year is None:
 		start_year = 1960
+	else:
+		start_year = int(start_year)
 	if end_year is None:
 		end_year = 2017
+	else:
+		end_year = int(end_year)
+
 	year_list = []
 	for i in range(start_year, end_year+1):
 		year_list.append("year_"+str(i))
