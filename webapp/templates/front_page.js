04ad6e4743f01ffd5d368f3c6efec0a4324e4882
thompsonw <wthompson@WFTASUS.localdomain>
2018-10-29 23:37:23 -0500

    Updated files

--- webapp/templates/front_page.js
+++ webapp/templates/front_page.js
@@ -1,39 +1,107 @@
-function clickedOnSelectYear(){
-    var selectYearButton = document.getElementById('select-years');
-    var years = document.getElementById('years');
-    if (selectYearButton.onclick == true) {
-      years.style.display = "block";
-    }
-    else{
-      years.style.display = "none";
-
-    }
-}
+var display = false;
+initialize();
 
 function initialize() {
-  var selectYearButton = document.getElementById('select-year');
-    selectYearButton.onclick = selectYearButton;
+    var selectYearButton = document.getElementById('select-year');
+    if (selectYearButton) {
+        selectYearButton.onclick = onSelectYearClicked;
+    }
+    getCountryListNavbar();
 }
 
- function getInputfromUser() {
-      var selectedCountries = [];
-      var country = document.getElementById('Algeria');
-      if (country.checked) {
-          selectedCountries.push(country);
-      }
+function onSelectYearClicked(){
+  if (!display){
+    var checkboxes = "";
+    for (var k = 1960; k < 2018; k++) {
+      checkboxes += '<input type="checkbox" name="yearCheckbox" value="'+k+'">'+k+'</input><br>';
+    }
 
-      var dataSelected = [];
-      var data = document.getElementById('gdp_usd');
-      if (data.checked) {
-          dataSelected.push(data);
+    var yearsDiv = document.getElementById('all-years');
+    if (yearsDiv) {
+      yearsDiv.innerHTML = checkboxes;
+    }
+  }
+    else {
+      var yearsDiv = document.getElementById('all-years');
+      if (yearsDiv) {
+        yearsDiv.innerHTML = "";
       }
-      SubmitButtonClicked(listOfCountries, listOfData);
+    }
+  display = !display;
+}
+
+function getCountryListNavbar() {
+    var url = "http://perlman.mathcs.carleton.edu:5104/countries/";
+    //var url = getBaseURL() + '/countries/';
+    fetch(url, {method: 'get'})
+    .then((response) => response.json())
+    .then(function(countries_list) {
+        var navbar = '';
+        for (var k = 0; k < countries_list.length; k++) {
+            navbar += '<li>';
+			      navbar += '<a href="https://www.google.com">'
+			      navbar += countries_list[k]['country_name']
+			      navbar += '</a>'
+			      navbar += '</li>'
+        }
+        var navbarElement = document.getElementById('vert-navbar');
+        if (navbarElement) {
+            navbarElement.innerHTML = navbar;
+        }
+    })
+
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
+ function getCountryInputfromUser() {
+   var selectedCountries = [];
+   var listOfCheckboxes = document.getElementsByName("countryCheckbox");
+   for (checkbox in listOfCheckboxes){
+       if (checkbox.checked == true){
+         selectedCountries.push(checkbox.value)
+       }
+   }
+   selectedCountries = selectedCountries.sort();
+   return selectedCountries;
   }
 
-  function SubmitButtonClicked(listOfCountries, listOfData) {
+  function getDataInputfromUser() {
+       var selectedData = [];
+       var listOfCheckboxes = document.getElementsByName("dataCheckbox");
+       for (checkbox in listOfCheckboxes){
+           if (checkbox.checked == true){
+             selectedData.push(checkbox.value)
+           }
+       }
+       selectedData = selectedData.sort();
+     return selectedData;
+   }
+
+   function getSpecifiedYearsfromUser() {
+
+        var selectedYears = [];
+        var listOfCheckboxes = document.getElementsByName("yearCheckbox");
+        var startYear = document.getElementById('start-year').value;
+        var endYear = document.getElementById('end-year').value;
+        for (checkbox in listOfCheckboxes){
+            if (checkbox.checked == true){
+              selectedYears.push(checkbox.value)
+            }
+        }
+        for (year = startYear; year <= endYear; year++){
+          selectedYears.push(year)
+        }
+        selectedYears = selectedYears.sort();
+      return selectedYears;
+    }
+
+
+  /*function SubmitButtonClicked() {
     url = getBaseURL() + "/data/";
 
-    if len(listOfCountries != 0){
+    if (listOfCountries.length != 0){
       for(country in listOfCountries) {
         url += "country_name=" + country + "&";
       }
@@ -88,5 +156,5 @@ function initialize() {
   function initialize() {
       var submit = document.getElementById('submit');
       submit.onclick = submit;
-  }
+  }*/
 window.onload = initialize;
